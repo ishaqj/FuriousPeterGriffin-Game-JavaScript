c086182f87c4dcc7c9842fdd52f93dcb47841c33
ishaq <ishaq_jound@hotmai.com>
2016-09-05 15:26:03 +0200

    first commit

---
+++ info.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang='sv'>
+<head>
+<meta charset='utf-8' />
+<title>Furious Peter Griffin</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class ='logo'>
+<img src="img/logo.png" alt="Peter Griffin">
+</div>
+<div class='container'>
+<h1>Furious Peter Griffin</h1>
+<p>Furious Peter Griffin är ett studentprojekt, utvecklat av Ishaq Jound, i kursen JavaScript vid Blekinge tekniska högskola.
+</p>
+<p>Furious Peter Griffin spelet går ut på att fånga 25 kycklingar inom 30 sekunder utan att missa någon kyckling, missar man att fånga någon kyckling slutar spelet och spelet slutar också om man inte lyckas fånga 25 kycklingar inom 30 sekunder.</p>
+<p>Kycklingen faller en efter en från himlen och det gäller att styra Peter så att han kan äta upp kycklingen innan den når golvet. Äter man upp kyckligen så dycker en ny fallande kyckling upp på en ny plats på spelplanen.</p>
+<p>Ju fler kycklingar du fångar desto snabbare börjar kycklingen att falla.</p>
+<p>Spelet styrs med piltangenterna.</p>
+<hr>
+<h1>Installation</h1>
+<p>Ladda ned spelet här: <a href="https://github.com/ishaqjound/FuriousPeterGriffin">Github</a></p>
+
+<p>Anslut sedan till en server och öppna spelet på din webbplats www.yourdomain.com/FuriousPeterGriffin/index.php och se så att allt fungerar.</p>
+<p>Så här kan det se ut efter installationen:</p>
+<p><a href="../furiouspetergriffin">Furious Peter Griffin Spel</a></p>
+<hr>
+<h1>Framtida plan för spelet</h1>
+<p>Följande funktioner kommer att finnas med till nästa versionen av spelet: </p>
+<ul>
+<li>En start knapp innan spelet börjar.</li> 
+<li>Ett highscore system.</li>
+<li>Pausa mitt i spelet.</li>
+<li>Fallande objekt som ger extra poäng.</li>
+ <li>Lägga till extra ljud när man har vunnit/förlorat.</li>
+</ul>
+</div>
+<script src="jquery.js"></script>
+<script src="main.js"></script>
+</body>
+</html>
